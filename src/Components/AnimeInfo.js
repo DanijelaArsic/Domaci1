6e010bea343b1d35335402446dbd37258c3e4a3b
Danijela Arsic <arsicdanijela8@gmail.com>
2023-12-19 20:50:35 +0100

    Promenjeno mesto synopsisa i promenjen css opet..

--- src/Components/AnimeInfo.js
+++ src/Components/AnimeInfo.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const AnimeInfo = (props) => {
-    const { title, images: { jpg: { large_image_url } }, rank, score, popularity } = props.animeInfo
+    const { title, images: { jpg: { large_image_url } }, rank, score, popularity, synopsis } = props.animeInfo
     return (
         <>
             <div className="anime-content">
@@ -10,7 +10,10 @@ export const AnimeInfo = (props) => {
                 <div className="info">
                     <h3>#Rank: {rank}</h3>
                     <h3>#Score: {score}</h3>
-                    <h3>#Popularity: {popularity}</h3> <hr /><br />
+                    <h3>#Popularity: {popularity}</h3>
+                    <h4>Synopsis: {synopsis}</h4>
+                    <hr /><br />
+
 
 
 
--- src/Components/AnimeList.js
+++ src/Components/AnimeList.js
@@ -1,26 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AnimeList = ({ animelist, setAnimeInfo }) => {
+    const [showSynopsis, setShowSynopsis] = useState(false);
+
+    const handleAnimeClick = (anime) => {
+        setAnimeInfo(anime);
+        setShowSynopsis(true);
+    };
+
     return (
         <>
-            {
-                animelist ? (
-                    animelist.map((anime, index) => {
-                        return (
-                            <div className="card" key={index} onClick={() => setAnimeInfo(anime)}>
-                                <img src={anime.images.jpg.large_image_url} alt="animeImage" />
-                                <div className="anime-info">
-                                    <h4>{anime.title}</h4>
-                                </div>
+            {animelist ? (
+                animelist.map((anime, index) => {
+                    return (
+                        <div className="card" key={index} onClick={() => handleAnimeClick(anime)}>
+                            <img src={anime.images.jpg.large_image_url} alt="animeImage" />
+                            <div className="anime-info">
+                                <h4>{anime.title}</h4>
+                                {showSynopsis && (
+                                    <div className="overlay">
+                                        <h4>{anime.title_japanese}</h4>
+
+                                    </div>
+                                )}
                             </div>
-
-                        )
-                    })
-                ) : "Not found"
-            }
-
-
+                        </div>
+                    );
+                })
+            ) : (
+                "Not found"
+            )}
         </>
-
-    )
-}
+    );
+};
