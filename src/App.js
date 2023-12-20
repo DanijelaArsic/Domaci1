2ca570f21c21a2707a651fa109ba44f4467e6339
Danijela Arsic <arsicdanijela8@gmail.com>
2023-12-20 15:10:54 +0100

    Dodato rutiranje?

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link, Redirect } from "react-router-dom";
 import './Components/style.css'
 import { AnimeList } from "./Components/AnimeList";
 import { AnimeInfo } from "./Components/AnimeInfo";
 import { AddToList } from "./Components/AddToList";
 import { RemoveFromList } from "./Components/RemoveFromList";
+import ContactPage from "./Components/ContactPage";
 
 function App() {
   const [search, setSearch] = useState("")
@@ -40,48 +42,61 @@ function App() {
   }, [search])
 
   return (
-    <><div className="header">
-      <h1>AnimePage</h1>
-      <div className="search-box">
-        <input type="search" placeholder="Search this page"
-          onChange={(e) => setSearch(e.target.value)} />
-      </div>
-      <div className="container">
-        <div className="animeInfo">
-          {animeInfo && <AnimeInfo animeInfo={animeInfo} />}
-        </div>
-
-      </div>
-      <div className="anime-row">
-        <h2 className="text-heading">Anime</h2>
-        <div className="row">
-          <AnimeList animelist={animeData}
-            setAnimeInfo={setAnimeInfo}
-            animeComponent={AddToList}
-            handleList={(anime) => addTo(anime)}
+    <Router>
+      <div className="header">
+        <h1>
+          <Link to="/">AnimePage</Link>
+        </h1>
+        <div className="search-box">
+          <input
+            type="search"
+            placeholder="Search this page"
+            onChange={(e) => setSearch(e.target.value)}
           />
         </div>
-        <h2 className="text-heading">My List</h2>
-        <div className="row">
-          <AnimeList animelist={myAnimeList}
-            setAnimeInfo={setAnimeInfo}
-            animeComponent={RemoveFromList}
-            handleList={(anime) => removeFrom(anime)}
-          />
+        <div>
+          <Link to="/contact">
+            <span>Contact</span>
+          </Link>
         </div>
       </div>
-    </div>
-    </>
-
-  )
-
-
-
-
-
-
-
+      <Routes>
+        <Route path="/contact" element={<ContactPage />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="container">
+                <div className="animeInfo">
+                  {animeInfo && <AnimeInfo animeInfo={animeInfo} />}
+                </div>
+              </div>
+              <div className="anime-row">
+                <h2 className="text-heading">Anime</h2>
+                <div className="row">
+                  <AnimeList
+                    animelist={animeData}
+                    setAnimeInfo={setAnimeInfo}
+                    animeComponent={AddToList}
+                    handleList={(anime) => addTo(anime)}
+                  />
+                </div>
+                <h2 className="text-heading">My List</h2>
+                <div className="row">
+                  <AnimeList
+                    animelist={myAnimeList}
+                    setAnimeInfo={setAnimeInfo}
+                    animeComponent={RemoveFromList}
+                    handleList={(anime) => removeFrom(anime)}
+                  />
+                </div>
+              </div>
+            </>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
-
-export default App
+export default App;
